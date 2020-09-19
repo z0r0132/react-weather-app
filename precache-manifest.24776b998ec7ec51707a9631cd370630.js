53f92829e19c92684f23eea70d45bdcc5d8cbfcb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-19 14:43:08 +0000

    Deploy brijesh-pant/react-weather-app to github.com/brijesh-pant/react-weather-app.git:gh-pages

---
+++ precache-manifest.24776b998ec7ec51707a9631cd370630.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5a0647fc1cab242e0318562867381b",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "71a175a85ef16eace24c",
+    "url": "/react-weather-app/static/css/2.301f0ffb.chunk.css"
+  },
+  {
+    "revision": "71a175a85ef16eace24c",
+    "url": "/react-weather-app/static/js/2.d26dba0e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-weather-app/static/js/2.d26dba0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8125dafa500a1158b558",
+    "url": "/react-weather-app/static/js/main.a5834f4c.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-weather-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-weather-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-weather-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-weather-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
